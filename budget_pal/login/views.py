e29fcce63f0e095d18359e1b56e4a79fbefb00b8
Carla Hayler <carla.hayler@ug.uchile.cl>
2023-05-11 17:53:02 -0400

    comentar código

--- budget_pal/login/views.py
+++ budget_pal/login/views.py
@@ -5,28 +5,28 @@ from .forms import NuevoRegistro
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 
-
+# registrar usuario
 def register_user(request):
     if request.method == 'GET':  # si estamos cargando la página
-        form = NuevoRegistro()
-        return render(request, "login/register_user.html", {'form': form}) # Mostrar el template con el formulario
+        form = NuevoRegistro() # formulario de forms.py
+        return render(request, "login/register_user.html", {'form': form}) # mostrar el template con el form
 
     elif request.method == 'POST':  # si estamos recibiendo el form de registro
         form = NuevoRegistro(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/login/login')
+        if form.is_valid(): # si los valores ingresados son validos 
+            form.save() # guarda en base de datos
+            return redirect('/login/login') # redirige a la página de login después de crear una cuenta
     else:
         form = NuevoRegistro()
-    return render(request, 'login/register_user.html', {'form': form})
+    return render(request, 'login/register_user.html', {'form': form}) # template de registro
 
 
-
+# ingresar usuario
 def login_user(request):
-    if request.method == 'GET':
-        return render(request, "login/login_user.html")
+    if request.method == 'GET':  # si estamos cargando la página
+        return render(request, "login/login_user.html") # mostrar el template
 
-    elif request.method == 'POST':
+    elif request.method == 'POST': # si estamos recibiendo el form de login
         nombre = request.POST['nombre']
         contraseña = request.POST['contraseña']
 
@@ -40,10 +40,10 @@ def login_user(request):
 
         if user is not None:
             login(request, user)
-            return redirect('/movimientos')  # redirige a la página de inicio después de iniciar sesión.
+            return redirect('/movimientos')  # redirige a la página de inicio después de iniciar sesión
         else:
             error_message = 'Usuario o contraseña incorrectos'
             return render(request, 'login/login_user.html', {'error_message': error_message})
 
     else:
-        return render(request, 'login/login_user.html')
+        return render(request, 'login/login_user.html') # template de login
