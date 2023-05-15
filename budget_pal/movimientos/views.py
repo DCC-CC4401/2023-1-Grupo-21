10c24c41aee91a5bff628e7f3efa0babb63ba351
Vonetto <juan.onetto@ug.uchile.cl>
2023-05-15 02:18:53 -0400

    Comments are added and indentation is corrected.

--- budget_pal/movimientos/views.py
+++ budget_pal/movimientos/views.py
@@ -10,17 +10,23 @@ from django.conf import settings
 
 
 # Create your views here.
+# Pagina principal
 def home(request):
+    # si el usuario fue autentificado, se procede a la pagina principal
     if request.user.is_authenticated:
+        # se filtran los datos que pertenecen al usuario desde la base de datos
         tabla = Movimientos.objects.filter(usuario=request.user)
+        # se crea la tabla que se va a mostrar en el html
         nueva_tabla = []
+        # por cada movimiento de la tabla, se crea su equivalente en la nueva tabla que se va a mostrar en el 
+        # html, manteniendo nombre_movimiento, categoria y fecha, agregando un tipo segun el monto (egreso si 
+        # es negativo, ingreso de caso contrario) y colocando el monto como valor absoluto
         for movimiento in tabla:
             if (movimiento.monto >= 0):
                 nuevo_registro = {
                     'nombre_movimiento': movimiento.nombre_movimiento,
                     'categoria': movimiento.categoria,
                     'fecha': movimiento.fecha,
-                    'usuario': movimiento.usuario,
                     'monto': movimiento.monto,
                     'tipo': 'Ingreso'
                 }
@@ -33,17 +39,21 @@ def home(request):
                     'tipo': 'Egreso'
                 }
             nueva_tabla.append(nuevo_registro)
+        # calculo la suma de todos los montos de la tabla original (para mostrar el saldo disponible)
         resultado = tabla.aggregate(Sum('monto'))
         suma_total = resultado['monto__sum']
+        # el saldo y los movimientos del usuario (de la nueva tabla) los pongo en el contexto para visualizarse
         context = {
             'movimientos': nueva_tabla,
             'saldo' : suma_total
         }
         return render(request, 'home.html', context)
     else:
+        # si el usuario no esta autentificado, se manda al login para que ingrese su usuario
         return HttpResponseRedirect(settings.LOGIN_URL)
 
 
+# Vista para desloguearse (usado en el boton de logout)
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(settings.LOGIN_URL)
