5f563cc90e5484d26bf4b7f14c34cb2e4844fee5
sven4 <salvador_727@hotmail.com>
2023-07-04 01:40:03 -0400

    Ahora la url base lleva al login

--- budget_pal/budget_pal/urls.py
+++ budget_pal/budget_pal/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('login.urls'), name='login'),
     path('admin/', admin.site.urls),
     path('movimientos/', include('movimientos.urls')),
     path('login/', include('login.urls')),
